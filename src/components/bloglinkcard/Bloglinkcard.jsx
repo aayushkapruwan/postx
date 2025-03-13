061f5a41eb24b827f921f9fcc0e66a933ebf860b
ayush singh kapruwan <kapruwanayush67@gmail.com>
2025-03-13 10:17:57 +0530

    ..

--- src/components/bloglinkcard/Bloglinkcard.jsx
+++ src/components/bloglinkcard/Bloglinkcard.jsx
@@ -1,15 +1,6 @@
-import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import postsimageobj from "../../appwrite/userpostimage.js";
 function Bloglinkcard({ $id, title, featuredimage }) {
-  // const [imgsrc,setimgsrc]=useState(null)
-  //   useEffect( ()=>{
-  //     async function getdata(){
-  //       const data=await postsimageobj.filepreview(featuredImage)
-  //     setimgsrc(data)
-  //     }
-  //     getdata()
-  //   },[])
+
   return (
     <Link to={`/post/${$id}`}>
       <div className=" rounded-2xl h-[300px] w-[280px] p-4 hover:text-white hover:shadow-[0px_0px_30px_10px_rgba(255,255,255,0.4)] transition-shadow duration-300">
